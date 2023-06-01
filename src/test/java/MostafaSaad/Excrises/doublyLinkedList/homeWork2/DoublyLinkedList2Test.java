0c98bf09213a6a8b744f5af6f5462ba9c5eaaaf3
hammo <hegazymohammed71@gmail.com>
2023-06-01 19:49:34 +0300

    HomeWork2 firstProplem

---
+++ src/test/java/MostafaSaad/Excrises/doublyLinkedList/homeWork2/DoublyLinkedList2Test.java
@@ -0,0 +1,36 @@
+package MostafaSaad.Excrises.doublyLinkedList.homeWork2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoublyLinkedList2Test {
+   private static DoublyLinkedList2 linkedList2=new DoublyLinkedList2();
+   private static List<Integer> list= List.of(1,2,3,4,5,6);
+    @Test
+    void findTheMiddle() {
+
+        linkedList2.clearLinkedList();
+        linkedList2.makeLinkedList(list);
+        assertEquals(linkedList2.findTheMiddle().data,4);
+        assertNotEquals(linkedList2.findTheMiddle().data,3);
+        linkedList2.clearLinkedList();
+        list=List.of(1,2);
+        linkedList2.makeLinkedList(list);
+        assertEquals(linkedList2.findTheMiddle().data,2);
+    }
+
+    @Test
+    public void  findTheMiddle2(){
+            linkedList2.clearLinkedList();
+            linkedList2.makeLinkedList(list);
+            assertEquals(linkedList2.findTheMiddle2().data,4);
+        assertNotEquals(linkedList2.findTheMiddle2().data,3);
+        linkedList2.clearLinkedList();
+        list=List.of(1,2);
+        linkedList2.makeLinkedList(list);
+        assertEquals(linkedList2.findTheMiddle2().data,2);
+    }
+}
