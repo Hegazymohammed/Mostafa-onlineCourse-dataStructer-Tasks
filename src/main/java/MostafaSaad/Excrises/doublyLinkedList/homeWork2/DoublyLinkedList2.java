57d5b44ae95db19b489b43142a0a22d2784130e4
hammo <hegazymohammed71@gmail.com>
2023-06-01 14:29:05 +0300

    HomeWork2 jun1 time

---
+++ src/main/java/MostafaSaad/Excrises/doublyLinkedList/homeWork2/DoublyLinkedList2.java
@@ -0,0 +1,9 @@
+package MostafaSaad.Excrises.doublyLinkedList.homeWork2;
+
+import MostafaSaad.Excrises.doublyLinkedList.DoublyLinkedList;
+
+public class DoublyLinkedList2 extends DoublyLinkedList {
+
+
+
+}
